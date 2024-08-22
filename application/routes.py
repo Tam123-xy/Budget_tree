dd9000a2efca8469d7fd88ec0410a57d4c368139
Tam123-xy <tanxinyi667@gmail.com>
2024-08-22 20:07:04 +0800

    I have link the piechart page to the navigation bar

--- application/routes.py
+++ application/routes.py
@@ -8,18 +8,9 @@ from application.models import IncomeExpenses
 def index():
     return render_template('index.html', title = 'index')
 
-# @app.route('/add', methods =['GET','POST'])
-# def add_expense():
-#     form= UserDataForm()
-#     if form.validate_on_submit():
-#         amount=form.amount.data
-#         category= form.category.data
-#         type= form.type.data 
-
-#         db.session.add(entry)
-#         db.session.commit()
-#         flash('Successful entry','success')
-#         return(url_for('index'))
+@app.route('/dashboard')
+def piechart():
+    return render_template('piechart.html', title = 'dashboard')
 
 @app.route('/add', methods =['GET','POST'])
 def Expense2():
