143a7dba5d3f29b96fdf2d84b2ce2f2da84e2b79
Tam123-xy <tanxinyi667@gmail.com>
2024-08-23 00:29:57 +0800

    I have done the database, data successfully save in database

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,8 @@
-from application import app, db
+from application.models import app, db
 from flask import render_template, flash, redirect, url_for, get_flashed_messages
-from application.form import UserDataForm
-# from flask_mysqldb import MySQL
-from application.models import IncomeExpenses
+from application.models import ExpenseForm, add_expenses
+from flask_sqlalchemy import SQLAlchemy
+
 
 @app.route('/')
 def index():
@@ -13,22 +13,21 @@ def piechart():
     return render_template('piechart.html', title = 'dashboard')
 
 @app.route('/add', methods =['GET','POST'])
-def Expense2():
-    return render_template('Expense2.html')
-    form= UserDataForm()
+def add_expense():
+    form = ExpenseForm()
     if form.validate_on_submit():
-        amount=form.amount.data
-        category= form.category.data
-        type= form.type.data 
-
-        db.session.add(entry)
-        db.session.commit()
-        flash('Successful entry','success')
-        return(url_for('index'))
-
-    if form.validate_on_submit():
-        print(f"Amount: {form.amount.data}, Type: {form.type.data}, Category: {form.category.data}")
-    # Then proceed with creating the entry
-
-    return render_template('add.html', title = 'layout', form= form)
-
+        if True:
+                entry = add_expenses(amount=form.amount.data, category=form.category.data, date=form.date.data)
+                db.session.add(entry)
+                db.session.commit()
+                amount = form.amount.data
+        # form.amount.data = ''
+        # form.category.data = ''
+        # form.date.data = ''
+        flash(f"RM{amount} has been added to expense record", "success")
+        # the_expense = add_expenses.query.order(add_expenses.id)
+        return redirect(url_for('index'))
+        
+    return render_template('add_expense.html', title="Add expenses", form=form)
+        
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from application import app
+from application.models import app
 
 if __name__ == '__main__':
     app.run(debug= True)
