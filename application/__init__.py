a2c6d9b5ab2ded0fd2a04d712b5841ae097f73fa
Tam123-xy <tanxinyi667@gmail.com>
2024-09-27 05:47:57 +0800

    done the account system

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
+
+bcrypt= Bcrypt(app)
 
 app.config['SECRET_KEY'] = "key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///income_expenseDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+
+
+
+
+
+
 
 from application import routes
